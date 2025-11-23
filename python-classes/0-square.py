6dd1be92bf880f7ed00d77974271a0a2433702c2
shukufa <shukufa@kali.kali>
2025-11-23 07:28:33 -0500

    python_classes

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
-"""Module for Square class."""
+"""Module that defines a Square class."""
 
 class Square:
+    """Class that defines a square by its private size attribute."""
+
     def __init__(self, size):
         self.__size = size
