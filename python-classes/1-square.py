ee6b78893bdbdf58da11f7c4868e5ec94324a45b
shukufa <shukufa@kali.kali>
2025-11-23 07:29:39 -0500

    python_classes

---
+++ python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Module that defines a Square class with validated size."""
+
+class Square:
+    """Class that defines a square with a private size attribute."""
+
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
